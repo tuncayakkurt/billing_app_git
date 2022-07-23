2bb3e2d30b668f103582c4c9fcf637af2a4d3337
tuncay akkurt <tuncayakkurt@yahoo.com>
2022-07-24 00:10:06 +0300

    ilk versiyon

--- service_parent/file_transfer_handler/src/main/java/com/billing/filetransferhandler/service/impl/FilesStorageServiceImpl.java
+++ service_parent/file_transfer_handler/src/main/java/com/billing/filetransferhandler/service/impl/FilesStorageServiceImpl.java
@@ -4,20 +4,34 @@ import com.billing.filetransferhandler.service.FileStorageService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 
 import static com.blg.client.constants.File.FILE_STORAGE_URL;
-
 @Service
 public class FilesStorageServiceImpl implements FileStorageService {
     private final Path root = Paths.get(FILE_STORAGE_URL);
 
     @Override
     public String save(MultipartFile file) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+            while (reader.ready()) {
+                String line = reader.readLine();
+                System.out.println(line);
+            }
+            return "";
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+        public String save_orig(MultipartFile file) {
         String uniqueName = createUniqueFilename(file.getOriginalFilename());
         try {
             Files.copy(file.getInputStream(), this.root.resolve(uniqueName));
